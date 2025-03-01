7e9c004738733e74cb655abf86761a5fe7ff00c2
Rick Alves <henryworkdev.ti@gmail.com>
2025-03-01 10:59:26 -0300

    adicionando status bar

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -4,7 +4,7 @@ import { View, StyleSheet } from 'react-native';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Oops! This screen doesn't exist." }} />
+      <Stack.Screen options={{ title: "Oops! Tela não encontrada!" }} />
       <View style={styles.container}>
       <Link href={"/" as any}>Home</Link>
       </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import { Stack } from 'expo-router/stack';
+import { StatusBar } from 'expo-status-bar';
+
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="dark" />
+    </>
+
   );
 }
